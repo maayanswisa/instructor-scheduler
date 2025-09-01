efcd2892193ce9d2dfd123285a98049eefc2161a
Maayan Swisa <120381527+maayanswisa@users.noreply.github.com>
2025-09-01 12:38:11 +0300

    first commit

---
+++ src/app/book/page.tsx
@@ -0,0 +1,8 @@
+
+export default function BookPage() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-semibold">עמוד הזמנת תור 📅</h1>
+    </main>
+  );
+}
